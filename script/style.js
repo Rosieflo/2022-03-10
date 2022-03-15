32e93d8441bd344c38b573282d81cbf462786adf
Rosieflo <97655399+Rosieflo@users.noreply.github.com>
2022-03-15 21:30:02 +0900

    Add files via upload

---
+++ script/style.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  let gnb = $('.gnb > ul > li');
+
+  gnb.mouseenter(function(){
+    $(this).find('.sub').stop().slideDown();
+  });
+
+  gnb.mouseleave(function(){
+    $('.sub').stop().slideUp();
+  });
+
+  
+});
